bb048bc7fc3c0ff04518d82b48bd8d1a6f1e674f
foisal404 <foisalahmmed2@gmail.com>
2023-05-04 00:46:09 +0600

    basic router set

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { router } from './route/Route/Route'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>hello world</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
