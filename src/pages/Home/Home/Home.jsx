9406d020458992451c95d8d55fdaf5cc00bf41ad
foisal404 <foisalahmmed2@gmail.com>
2023-05-04 01:48:33 +0600

    hero section banner update

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
                 <div className="hero-overlay bg-opacity-60"></div>
                     <div className="hero-content text-center text-neutral-content">
                         <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="mb-5 text-5xl font-bold">Let your taste buds travel to Turkey with <br /> <span className='text-red-400'>Master Cuis<span className='bg-gray-200 '>in</span>e.</span></h1>
+                        <p className="mb-5 ">Discover the flavors of Turkish cuisine with Master Cuisine! Our platform brings together a community of talented chefs and food enthusiasts, who share their passion for delicious food and culinary creativity.</p>
+                        <button className="btn btn-error text-white">Get Started</button>
                         </div>
                     </div>
                 </div>
