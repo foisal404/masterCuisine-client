b43a4e49d7019e46a225db46a4741dc5f37e1f6d
foisal404 <foisalahmmed2@gmail.com>
2023-05-04 02:52:16 +0600

    chefCard update and some styles update

--- src/pages/Home/Chefcard/ChefCard.jsx
+++ src/pages/Home/Chefcard/ChefCard.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React from "react";import { FaRegThumbsUp } from "react-icons/fa";
+const ChefCard = ({ data }) => {
+  const {
+    bio,
+    likes,
+    name,
+    number_of_recipes,
+    picture,
+    recipes,
+    years_experience,
+  } = data;
 
-const ChefCard = ({data}) => {
-    const {bio,likes,name,number_of_recipes,picture,recipes,years_experience}=data;
-    
-    return (
-        <div className='border'>
-            {name}
+  return (
+    <div className="">
+      <div className="card w-96 mx-auto my-8 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src={picture}
+            className="h-80 w-full"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">{name}</h2>
+            <p>Exprience: {years_experience} years</p>
+            <p>Number of recipes {number_of_recipes} </p>
+            <div className="flex">
+                <FaRegThumbsUp></FaRegThumbsUp>
+                <p>{likes} </p>
+            </div>
+            <div className="card-actions">
+                <button className="btn btn-error">View Recipes</button>
+            </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ChefCard;
