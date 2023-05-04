8f58e2998ec2fef3983e9ccbf85b11f6b13e5548
foisal404 <foisalahmmed2@gmail.com>
2023-05-04 18:01:36 +0600

    onStateAuthChange add to state change monitor

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 export const AuthContext=createContext(null);
@@ -8,6 +8,8 @@ const AuthProvider = ({children}) => {
     const gitProvider = new GithubAuthProvider();
     const googleProvider = new GoogleAuthProvider();
     const auth = getAuth(app);
+
+    const [user,setUser]=useState(null)
     const githubIN=()=>{
         return signInWithPopup(auth,gitProvider)
     }
@@ -21,7 +23,18 @@ const AuthProvider = ({children}) => {
     const logIn=(email,password)=>{
         return signInWithEmailAndPassword(auth,email,password)
     }
+    //state change monitorer
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser)
+            console.log('state change');
+        })
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
     const authinfo={
+        user,
         githubIN,
         googleIN,
         SignUp,
