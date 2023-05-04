89562e3e45c611759785512384f591fdd91038bb
foisal404 <foisalahmmed2@gmail.com>
2023-05-04 16:47:26 +0600

    firebase insatal and congig fiel set

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaS9iuQIGfTh_DUQM_oYxoqPOY9k_6Yh0",
+  authDomain: "master-cuisine-336f3.firebaseapp.com",
+  projectId: "master-cuisine-336f3",
+  storageBucket: "master-cuisine-336f3.appspot.com",
+  messagingSenderId: "90326020856",
+  appId: "1:90326020856:web:01f610b989b71dd6f9010d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
